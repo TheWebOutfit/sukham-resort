9c153a058a2c03849149722462b3490ce056355e
Rohan Singh <rohannsingh98765@gmail.com>
2023-05-06 21:35:44 +0530

    folder and file structure
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between content-center items-center  text-center bg-red-200'>
+    <div className='flex flex-col sticky z-10 sm:flex-row justify-between content-center items-center  text-center bg-red-200'>
         <div className='font-bold text-4xl p-3 '>
             Sukham Resort
         </div>
 
         <div className='flex '>
+            
             <div className='m-3'>
                 Home
             </div>
