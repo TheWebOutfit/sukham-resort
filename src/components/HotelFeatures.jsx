35a32523f976532cc65cd317e21d8d330888e40b
Rohan Singh <rohannsingh98765@gmail.com>
2023-05-07 11:07:34 +0530

    Gallery view
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

---
+++ src/components/HotelFeatures.jsx
@@ -0,0 +1,18 @@
+import PoolIcon from '@mui/icons-material/Pool';
+import RoomServiceIcon from '@mui/icons-material/RoomService';
+import FoodBankIcon from '@mui/icons-material/FoodBank';
+import LocalCafeIcon from '@mui/icons-material/LocalCafe';
+
+const HotelFeatures = () => {
+  return (
+    <div className='flex justify-center p-5'>
+        <PoolIcon fontSize="large" />
+        <RoomServiceIcon fontSize="large" />
+        <FoodBankIcon fontSize="large" />
+        <LocalCafeIcon fontSize="large" />
+        
+    </div>
+  )
+}
+
+export default HotelFeatures
