d5ebf8226be4e217918c52fd1284b93a51f73087
Mohit030802 <mohitom2002@gmail.com>
2023-05-18 01:25:42 +0530

    About us section done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,13 @@ import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
+    const handleClickScroll = () => {
+        const element = document.getElementById('about');
+        if (element) {
+          
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
   return (
     <div className='flex flex-col sticky z-10 sm:flex-row justify-between content-center items-center  text-center bg-gradient-to-r from-[#8294C4] via-[#DBDFEA] to-[#D5B4B4]'>
         <div className='font-bold text-4xl p-3  uppercase '>
@@ -16,7 +23,7 @@ const Navbar = () => {
                 Home
                 </Link>
             </div>
-            <div className='m-1 sm:m-3'>
+            <div className='m-1 sm:m-3' onClick={handleClickScroll}>
                 About Us
             </div>
             <div className='m-1 sm:m-3'>
