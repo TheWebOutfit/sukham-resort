df69e63799a6c11e14a4aa162b8e3afb72f0285c
Mohit030802 <mohitom2002@gmail.com>
2023-05-18 01:18:10 +0530

    About us section added

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,18 +5,16 @@ import HomeIcon from "@mui/icons-material/Home";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import emailjs from '@emailjs/browser';
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
 
 const ContactForm = () => {
   const handleClickScroll = () => {
-    const element = document.getElementById('maps');
+    const element = document.getElementById("maps");
     if (element) {
-     
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
   };
-  
 
   return (
     <>
@@ -61,17 +59,23 @@ const ContactForm = () => {
                 </a>
               </div>
               <div className="inline-flexspace-x-2 items-center">
-                
-                 
-                  <button onClick={handleClickScroll} className="bg-white text-black rounded-lg shadow-lg px-6 py-4 align-middle font-bold text-xl hover:bg-gradient-to-r from-[#434343] to-[#000000] hover:text-white">Find Us <ArrowForwardIcon className="hover:rotate-90"/></button>
-                </div>
-              
+                <button
+                  onClick={handleClickScroll}
+                  className="bg-white text-black rounded-lg shadow-lg px-6 py-4 align-middle font-bold text-xl hover:bg-gradient-to-r from-[#434343] to-[#000000] hover:text-white"
+                >
+                  Find Us <ArrowForwardIcon className="hover:rotate-90" />
+                </button>
+              </div>
             </div>
-            
-            <div> 
+
+            <div>
               <div className="relative">
                 <div className="bg-white bg-opacity-50 rounded-xl p-8 text-gray-800 md:w-full">
-                  <form action="https://formspree.io/f/mpzebnra" method="POST" className="flex flex-col space-y-6">
+                  <form
+                    action="https://formspree.io/f/mpzebnra"
+                    method="POST"
+                    className="flex flex-col space-y-6"
+                  >
                     <div>
                       <label
                         htmlFor=""
@@ -118,7 +122,7 @@ const ContactForm = () => {
                         className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2  outline-none focus:ring-2 focus:ring-gray-800"
                       />
                     </div>
-                    <button  className=" bg-black text-white font-bold rounded-lg px-8 py-4 uppercase text-sm align-middle items-center justify-center hover:bg-white hover:text-black">
+                    <button className=" bg-black text-white font-bold rounded-lg px-8 py-4 uppercase text-sm align-middle items-center justify-center hover:bg-white hover:text-black">
                       Send Message
                     </button>
                   </form>
@@ -126,21 +130,22 @@ const ContactForm = () => {
               </div>
             </div>
           </div>
-         
         </div>
-        <div className="flex flex-col items-center justify-center p-8 rounded-xl shadow-lg " id="maps">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7341.141332915838!2d76.86034987329806!3d23.07619663378666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1683455187108!5m2!1sen!2sin"
-          width="90%"
-          height="450"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+        <div
+          className="flex flex-col items-center justify-center p-8 rounded-xl shadow-lg "
+          id="maps"
+        >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7341.141332915838!2d76.86034987329806!3d23.07619663378666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1683455187108!5m2!1sen!2sin"
+            width="90%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
-      </div>
-      
     </>
   );
 };
