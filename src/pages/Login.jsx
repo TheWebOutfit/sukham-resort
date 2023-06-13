92ecf18947c221e625b12fac399041e071232bc4
Mohit030802 <mohitom2002@gmail.com>
2023-06-14 02:43:34 +0530

    Login signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,62 +1,91 @@
 import { useState } from 'react';
 import { TailSpin } from 'react-loader-spinner';
+import { Link } from 'react-router-dom';
 
-const idPass =
-    {
-        name: "Rohan",
-        password: "rohan"
-    }
+// const idPass =
+//     {
+//         name: "Rohan",
+//         password: "rohan"
+//     }
 
 
 const Login = ({setLogin}) => {
-    const [form, setForm] = useState({
-        name: "",
-        password: ""
-      });
-      const [loading, setLoading] = useState(false);
+    // const [form, setForm] = useState({
+    //     name: "",
+    //     password: ""
+    //   });
+    //   const [loading, setLoading] = useState(false);
       
 
   return (
-    <div className="w-full text-black h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#B7B7B7] via-[#B9E9FC] to-[#F6F1F1] ">
-    <h1 className="text-2xl font-bold">Login</h1>
-    <div className="m-2 w-full md:w-1/3">
-      <div className="relative">
-        <label for="message" className="leading-7 text-sm text-bold">
-          Mobile No.
-        </label>
-        <input
-          type={"text"}
-          value={form.name}
-          onChange={(e) => setForm({ ...form, name: e.target.value })}
-          className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        />
-      </div>
+  //   <div className="w-full text-black h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#B7B7B7] via-[#B9E9FC] to-[#F6F1F1] ">
+  //   <h1 className="text-2xl font-bold">Login</h1>
+  //   <div className="m-2 w-full md:w-1/3">
+  //     <div className="relative">
+  //       <label for="message" className="leading-7 text-sm text-bold">
+  //         Mobile No.
+  //       </label>
+  //       <input
+  //         type={"text"}
+  //         value={form.name}
+  //         onChange={(e) => setForm({ ...form, name: e.target.value })}
+  //         className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+  //       />
+  //     </div>
+  //   </div>
+  //   <div className="p-2 w-full md:w-1/3">
+  //     <div className="relative">
+  //       <label for="message" className="leading-7 text-sm text">
+  //         Password
+  //       </label>
+  //       <input
+  //         value={form.password}
+  //         onChange={(e) => setForm({ ...form, password: e.target.value })}
+  //         className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+  //       />
+  //     </div>
+  //   </div>
+  //   <div className="p-2 w-full">
+  //     <button
+  //     onClick={() => form.name == idPass.name && form.password == idPass.password ?
+  //       setLogin(true) : alert(`Wrong PassWord the coreect password is ${idPass.password}`)}
+  //       className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
+  //     >
+  //       {loading ? <TailSpin height={25} color="white" /> : "Login"}
+  //     </button>
+  //   </div>
+  //   <div>
+  //     {/* <p>Do not have account? <Link to={'/signup'}><span classNameName="text-blue-500">Sign Up</span></Link></p> */}
+  //   </div>
+  // </div>
+  <>
+  <div className='bg-gray-100'>
+
+  
+  <div class="container  flex  mx-auto items-center justify-center ">
+        <div class="left w-1/3 mx-14">
+            
+            <p class="text-3xl mx-5 font-paraFont mb-3">Welcome to </p>
+            <p class="text-6xl mx-5 font-paraFont "><span className='text-gold'>Sukham </span>Resort</p>
+        </div>
+        <div class="right flex flex-col bg-white p-8 rounded-lg w-1/4 relative mt-8 mb-8">
+          <h1 className='flex text-3xl items-center justify-center font-bold mb-6'>Login </h1>
+            <input class="px-4 h-12 my-2 border border-1  outline-blue-200 border-gray-200 rounded-lg" type="text" placeholder="Enter Your Email address here" />
+            <input class="px-4 h-12 my-2 border border-1 outline-blue-200 border-gray-200 rounded-lg" type="password" placeholder="Enter Your password" />
+            <button class="bg-blue-600 hover:bg-blue-700 text-white py-3 my-2 rounded-md font-bold cursor-pointer ">Log In</button>
+            <span class="text-blue-600 text-center text-sm my-2 py-3 cursor-pointer hover:underline">Forgot password</span>
+            <hr class="my-2" />
+            
+            <button class="bg-gray-600 hover:bg-green-700 text-white py-3 my-2 rounded-md font-bold cursor-pointer w-fit px-4 mx-auto"><Link to='/Sign'>Create New Account</Link></button>
+        </div>
     </div>
-    <div className="p-2 w-full md:w-1/3">
-      <div className="relative">
-        <label for="message" className="leading-7 text-sm text">
-          Password
-        </label>
-        <input
-          value={form.password}
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
-          className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        />
-      </div>
     </div>
-    <div className="p-2 w-full">
-      <button
-      onClick={() => form.name == idPass.name && form.password == idPass.password ?
-        setLogin(true) : alert(`Wrong PassWord the coreect password is ${idPass.password}`)}
-        className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
-      >
-        {loading ? <TailSpin height={25} color="white" /> : "Login"}
-      </button>
-    </div>
-    <div>
-      {/* <p>Do not have account? <Link to={'/signup'}><span classNameName="text-blue-500">Sign Up</span></Link></p> */}
-    </div>
-  </div>
+    </>
+  
+
+  
+  
+  
   )
 }
 
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  return (
+    <div className=" flex bg-gray-100 justify-center items-center">
+      <div class="flex flex-col bg-white p-8 rounded-lg w-1/2 relative mt-8 mb-8">
+        <h1 className="flex text-3xl items-center justify-center font-bold mb-6">
+          Sign Up{" "}
+        </h1>
+        <div className="flex justify-center items-center mt-4">
+          <form class="w-full max-w-lg">
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-first-name"
+                >
+                  First Name
+                </label>
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="grid-first-name"
+                  type="text"
+                  placeholder="Jane"
+                />
+                <p class="text-red-500 text-xs italic">
+                  Please fill out this field.
+                </p>
+              </div>
+              <div class="w-full md:w-1/2 px-3">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-last-name"
+                >
+                  Last Name
+                </label>
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-last-name"
+                  type="text"
+                  placeholder="Doe"
+                />
+              </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-2">
+              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-city"
+                >
+                  Age
+                </label>
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-city"
+                  type="number"
+                  placeholder="5"
+                />
+              </div>
+              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-state"
+                >
+                  Gender
+                </label>
+                <div class="relative">
+                  <select
+                    class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-state"
+                  >
+                    <option>Male</option>
+                    <option>Female</option>
+                    <option>Others</option>
+                  </select>
+                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg
+                      class="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-zip"
+                >
+                  Date of Birth
+                </label>
+
+                <div class="relative max-w-sm">
+                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      aria-hidden="true"
+                      class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                  <input
+                    datepicker
+                    type="text"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Select date"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-password"
+                >
+                  Email
+                </label>
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-password"
+                  type="password"
+                  placeholder="Enter Your Email Here!!!"
+                />
+              </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-password"
+                >
+                  Password
+                </label>
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-password"
+                  type="password"
+                  placeholder="******************"
+                />
+                <p class="text-gray-600 text-xs italic">
+                  Make it as long and as crazy as you'd like
+                </p>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
